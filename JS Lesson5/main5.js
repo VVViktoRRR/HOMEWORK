bcb47f5fca009060d34a9cead14adea0164a9202
VVViktoRRR <86650093+VVViktoRRR@users.noreply.github.com>
2021-07-27 12:36:46 +0300

    Add files via upload

--- JS Lesson5/main5.js
+++ JS Lesson5/main5.js
@@ -1,56 +1,51 @@
 // - Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
 // -- отримує текст з параграфа з id "content"
-    document.getElementById('content');
-    console.log(content);
+document.getElementById('content');
+console.log(content);
 
 // -- отримує текст з блоку з id "rules"
-    document.getElementById('rules');
-    console.log(rules);
+document.getElementById('rules');
+console.log(rules);
 
 // -- замініть текст параграфа з id 'content' на будь-який інший
 
-    content.innerText = ' Понедельник - день тяжелый, как бы всё успеть ? На работу нужно ехать, ну и лекции смотреть. ' +
-        'Д.З. 5 хочу всю сделать.'
+content.innerText = ' Понедельник - день тяжелый, как бы всё успеть ? На работу нужно ехать, ну и лекции смотреть. ' +
+    'Д.З. 5 хочу всю сделать.'
 
 // -- замініть текст параграфа з id 'rules' на будь-який інший
 
-    rules.innerText = 'Але таке життя, воно бентежне. Завжди потрібно прагнути до кращого, нажаль це не легко'
+rules.innerText = 'Але таке життя, воно бентежне. Завжди потрібно прагнути до кращого, нажаль це не легко'
 
 // -- змініть кожному елементу колір фону на червоний
-        let pLists = document.body.getElementsByTagName('p');
-        console.log(pLists);
-        for (const pListElement of pLists) {
-        pListElement.style.background = 'red';
-       }
-    let classLists = document.body.getElementsByClassName('fc bp');
-        console.log(classLists);
+let pLists = document.body.getElementsByTagName('p');
+console.log(pLists);
+for (const pListElement of pLists) {
+    pListElement.style.background = 'red';
+}
+let classLists = document.body.getElementsByClassName('fc bp');
+console.log(classLists);
 
-    for (const classListElement of classLists) {
-        classListElement.style.background = 'red';
+for (const classListElement of classLists) {
+    classListElement.style.background = 'red';
 }
 // -- змініть кожному елементу колір тексту на синій
-    for (const pListElement of pLists) {
-        pListElement.style.color = 'blue';
-    }
-    for (const classListElement of classLists) {
-        classListElement.style.color = 'blue';
+for (const pListElement of pLists) {
+    pListElement.style.color = 'blue';
+}
+for (const classListElement of classLists) {
+    classListElement.style.color = 'blue';
 }
 // -- отримати весь список класів елемента з id=rules і вивести їх в console.log
-    document.getElementById('rules');
-    console.log(rules.innerText);
+document.getElementById('rules');
+console.log(rules.innerText);
 
 // -- отримати всі елементи з класом fc_rules. визначити їм 2 події "клік", одна вивдоить текст елементу, інша довільний ьексь
-    let classLists2 = document.getElementsByClassName('fc_rules');
-        console.log(classLists2);
-    for (let classLists2Element of classLists2) {
-    classLists2Element.click = () => console.log(classLists2Element.textContent);
-    classLists2Element.click = () => console.log(classLists2Element.innerHTML = 'Hello');
+let classLists2 = document.getElementsByClassName('fc_rules');
+console.log(classLists2);
+for (let classLists2Element of classLists2) {
+    classLists2Element.onclick = () => console.log(classLists2Element.textContent);
+    classLists2Element.onclick = () => console.log(classLists2Element.innerHTML = 'Hello');
 }
-    // classLists2Element.onclick(function () {
-    //        console.log(classLists2Element.innerText)}
-    // classLists2Element.onclick(function () {
-    //         console.log(`Hello`)}
-    // });
 
 // -- поміняти колір тексту у всіх елементів fc_rules на червоний
         for (const classListElement2 of classLists2) {
